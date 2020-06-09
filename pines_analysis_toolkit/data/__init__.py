42eff44d396c47186ad34216b31ef4520321e00e
patricktamburo <tamburop@bu.edu>
2020-06-09 16:59:04 -0400

    updated reduce.py

--- pines_analysis_toolkit/data/__init__.py
+++ pines_analysis_toolkit/data/__init__.py
@@ -2,3 +2,6 @@ from pines_analysis_toolkit.data.get_raw_science_files import get_raw_science_fi
 from pines_analysis_toolkit.data.move_science_files import move_science_files
 from pines_analysis_toolkit.data.dome_flat_field import dome_flat_field
 from pines_analysis_toolkit.data.dark import dark
+from pines_analysis_toolkit.data.bpm_maker import bpm_maker
+from pines_analysis_toolkit.data.get_master_dome_flats import get_master_dome_flats
+from pines_analysis_toolkit.data.get_master_darks import get_master_darks
---
+++ reduce_test.py
@@ -0,0 +1,5 @@
+import pines_analysis_toolkit as pat
+
+target = '2MASS J16291840+0335371'
+
+pat.reduction.reduce(target, upload='True')
