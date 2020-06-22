a1d2a05183e00ea6ddf0972598990f453373e7f0
patricktamburo <tamburop@bu.edu>
2020-06-22 09:27:35 -0400

    get_reduced_science_files fix

--- tests/photometry_tester.py
+++ tests/photometry_tester.py
@@ -1,10 +1,22 @@
 import pines_analysis_toolkit as pat
-import pdb
 
+#Declare a target.
 target = '2MASS J16291840+0335371'
 
-targ_and_refs = pat.photometry.ref_star_chooser(target, restore=True)
-centroided_sources = pat.photometry.centroider(target, targ_and_refs, restore=True)
-aper_phot = pat.photometry.aper_phot(target, centroided_sources, [5.,6.,7.,8.,9.])
+#Set up connection to pines.bu.edu.
+sftp = pat.utils.pines_login()
 
-pdb.set_trace()
+#Grab reduced data for target. 
+pat.data.get_reduced_science_files(sftp, target)
+
+#Close sftp connection. 
+sftp.close()
+
+#Detect target and suitable reference stars. 
+targ_and_refs = pat.photometry.ref_star_chooser(target, restore=False, lower_left=True)
+
+#Centroid target and references. 
+centroided_sources = pat.photometry.centroider(target, targ_and_refs, restore=False)
+
+#Do aperture photometry on target and references. 
+pat.photometry.aper_phot(target, centroided_sources, [5.,6.,7.,8.,9.])
