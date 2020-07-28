83891be6ebc6675fc648e9aae1b9ad066370133b
patricktamburo <46655539+patricktamburo@users.noreply.github.com>
2020-07-28 11:57:18 -0400

    Delete lightcurve_tester.py

--- lightcurve_tester.py
+++
@@ -1,9 +0,0 @@
-import pines_analysis_toolkit as pat
-import pdb
-
-target = '2MASS J17281134+0839590'
-
-sources = pat.photometry.ref_star_chooser(target, restore=True)
-pat.analysis.lightcurve(target, sources)
-
-pdb.set_trace()
