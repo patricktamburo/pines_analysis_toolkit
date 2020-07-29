3d596071c2ae28c140368193108112104102ef2c
patricktamburo <tamburop@bu.edu>
2020-07-29 13:52:41 -0400

    updated short_name_creator

--- pines_analysis_toolkit/utils/short_name_creator.py
+++ pines_analysis_toolkit/utils/short_name_creator.py
@@ -1,4 +1,5 @@
 import pdb
+import time
 def short_name_creator(long_name):
     long_name = long_name.replace(' ','')
     if '+' in long_name:
@@ -6,5 +7,9 @@ def short_name_creator(long_name):
     elif '-' in long_name:
         short_name = '2MASS '+long_name.split('J')[1].split('-')[0][0:4]+'-'+long_name.split('J')[1].split('-')[1][0:4]
     else:
-        print('ERROR: Is this long_name correct?')
+        print('')
+        print('WARNING: long_name does not match 2MASS Jxxxxxxxx+xxxxxxx format, returning long_name.')
+        time.sleep(3)
+        short_name = long_name
+    
     return short_name
